2586611f90f5dc5d676a315bab506f073a91a2a1
LakshmiRavali <lakshmiravali.rimmalapudi@gmail.com>
2021-08-22 14:29:26 +0530

    fix: Final Update

--- scripts/update-api-definitions.js
+++ scripts/update-api-definitions.js
@@ -4,12 +4,7 @@ const { ChangeLogHelper } = require('./change-log-helper');
 const getAndUpdateChangeLogType = async () => {
   const versionRegex = /(\d+)\.(\d+)\.(\d+)/;
   const dateRegex = /\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])/;
-  const ch = new ChangeLogHelper(
-    versionRegex,
-    dateRegex,
-    'CHANGES.md',
-    'OAI_CHANGES.md',
-  );
+  const ch = new ChangeLogHelper(versionRegex, dateRegex, 'CHANGES.md', 'OAI_CHANGES.md');
   await ch.appendChangesToChangelog();
   const versions = await ch.getFirstTwoVersions();
   if (versions.length === 2) {
