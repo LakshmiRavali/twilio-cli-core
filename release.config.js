b3727567d4d3eba7f8d134cb66356ecc08d04032
LakshmiRavali <lakshmiravali.rimmalapudi@gmail.com>
2021-08-24 15:49:36 +0530

    fix: Created config file for semantic release

---
+++ release.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  branches: [
+    'test_branch_actions',
+    {
+      name: 'beta',
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGES.md',
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGES.md', 'package.json'],
+        // eslint-disable-next-line no-template-curly-in-string
+        message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
