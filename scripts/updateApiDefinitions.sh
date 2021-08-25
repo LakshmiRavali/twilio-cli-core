749a3168046e66dcbd2475be28ae1fc5c16947b6
lakshmiravali <lakshmiravali.rimmalapudi@gmail.com>
2021-08-25 16:18:17 +0530

    fix: Added changes in package.json and Updated scriot file

--- scripts/updateApiDefinitions.sh
+++ scripts/updateApiDefinitions.sh
@@ -3,11 +3,11 @@ echo "Copying api-definitions"
 cp -R ~/oai_definitions/json/. src/services/twilio-api/
 echo "Running update changelog script"
 changeLog=$(node scripts/update-api-definitions.js)
-changeLog="${changeLog//'%'/'%25'}"
-changeLog="${changeLog//$'\n'/'%0A'}"
-changeLog="${changeLog//$'\r'/'%0D'}"
-echo "CHANGE_LOG=$changeLog" >> $GITHUB_ENV
-# echo "::set-output name=changeLog::$changeLog"
+if [ "$changeLog" != '' ]; then
+  changeLog="${changeLog//'%'/'%25'}"
+  changeLog="${changeLog//$'\n'/'%0A'}"
+  changeLog="${changeLog//$'\r'/'%0D'}"
+fi
 versionType=$(node scripts/get-version-type.js)
 echo "Version type: $versionType"
 rm -rf OAI_CHANGES.md
@@ -16,17 +16,23 @@ git config --global user.email "lakshmiravali.rimmalapudi@gmail.com"
 git config --global user.name "lakshmiravali"
 branch=$(git branch --show-current)
 echo "Current branch: $branch"
-git add .
-commitMessage=''
-if [ $versionType == 0 ] || [ $versionType == 1 ]
-then
-  commitMessage='doc: Updated api definitions'
-elif [ $versionType == 2 ]
-then
-  commitMessage='fix: Updated api definitions'
+git add -A
+if [ -n "$(git status --porcelain)" ]; then
+  echo "There are changes to commit.";
+  commitMessage=''
+  if [ "$versionType" == 0 ] || [ "$versionType" == 1 ]
+  then
+    commitMessage='feat: Updated api definitions'
+  elif [ "$versionType" == 2 ]
+  then
+    commitMessage='fix: Updated api definitions'
+  else
+    echo "Invalid versionType: $versionType";
+    exit
+  fi
+  echo "Commit message:$commitMessage"
+  git commit -m "$commitMessage"
+  git push origin "$branch"
 else
-  exit
+  echo "No OpenAPI changes to commit";
 fi
-echo "Commit message:$commitMessage"
-git commit -m "$commitMessage"
-git push origin $branch
